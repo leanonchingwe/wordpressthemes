128cab4569c9c942b7572e604ef751213a65947d
leanonchingwe <66792863+leanonchingwe@users.noreply.github.com>
2023-03-05 06:08:32 +0200

    Create footer.php

---
+++ my-folder/footer.php
@@ -0,0 +1,16 @@
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All rights reserved.</p>
+      </div>
+      <div class="col-md-6 text-right">
+        <ul class="social-links">
+          <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+          <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+          <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
